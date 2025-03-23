777994d7e1bbcf02174edb3df16327d946d4ea7a
Yird <100542617+Y1rd@users.noreply.github.com>
2025-03-23 02:21:51 -0700

    Initial commit, Has Villager-Golem Co-existing feature

---
+++ src/main/java/birb/yird/cmctweaks/mixins/GolemAttackPatch.java
@@ -0,0 +1,29 @@
+package birb.yird.cmctweaks.mixins;
+
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.goal.target.DefendVillageTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.animal.IronGolem;
+import net.minecraft.world.entity.monster.Creeper;
+import net.minecraft.world.entity.monster.Enemy;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(IronGolem.class)
+public class GolemAttackPatch {
+
+    @Inject(method = "registerGoals", at = @At("TAIL"))
+    protected void overrideRegisterGoals(CallbackInfo ci) {
+        IronGolem self = (IronGolem) (Object) this;
+        // Remove all target selection goals
+        self.targetSelector.getAvailableGoals().removeIf(goal -> self.targetSelector.getAvailableGoals().contains(goal));
+        // Re-add the player attack goals
+        self.targetSelector.addGoal(1, new DefendVillageTargetGoal(self));
+        self.targetSelector.addGoal(2, new HurtByTargetGoal(self));
+        // Re-add the hostile mob targeting goal
+        self.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(self, Mob.class, 5, false, false, (p_28879_) -> p_28879_ instanceof Enemy && !(p_28879_ instanceof Creeper)));
+    }
+}
---
+++ src/main/java/birb/yird/cmctweaks/mixins/VillagerFearPatch.java
@@ -0,0 +1,26 @@
+package birb.yird.cmctweaks.mixins;
+
+import net.ltxprogrammer.changed.entity.variant.TransfurVariant;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Mixin(TransfurVariant.Builder.class)
+public class VillagerFearPatch {
+
+    @ModifyArg(
+            method = "<init>",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Ljava/util/ArrayList;<init>(Ljava/util/Collection;)V"
+            ),
+            index = 0
+    )
+    // Return an empty array so villagers are no longer scared.
+    private <E> Collection<? extends E> fearlessVillager(Collection<? extends E> c) {
+        return new ArrayList<>();
+    }
+}
